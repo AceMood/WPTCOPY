27d498838faba8440a20604ce3115f24452b6d0f
zmike86 <zmike86@163.com>
2014-07-07 09:57:34 -0700

    coommit two

--- savePerfData.php
+++ savePerfData.php
@@ -8,36 +8,36 @@ $benchmarkResults = formatResults($_POST["data"]);
 saveLog($benchmarkResults, $logfile);
 
 function formatResults($r) {
-	$r = stripcslashes($r);
-	$r = json_decode($r);
-	if (json_last_error() > 0) {
-		die("invalid json");
-	}
-	return $r;
+    $r = stripcslashes($r);
+    $r = json_decode($r);
+    if (json_last_error() > 0) {
+        die("invalid json");
+    }
+    return $r;
 }
 
 function formatNewLog($file) {
-	$headerline = "IP, TestID, StartTime, StopTime, RunTime, URL, UserAgent";
-	appendToFile($headerline, $file);
+    $headerline = "IP, TestID, StartTime, StopTime, RunTime, URL, UserAgent";
+    appendToFile($headerline, $file);
 }
 
 function saveLog($obj, $file) {
-	if (!file_exists($file)) {
-		formatNewLog($file);
-	}
-	$obj->useragent = cleanCommas($obj->useragent);
+    if (!file_exists($file)) {
+        formatNewLog($file);
+    }
+    $obj->useragent = cleanCommas($obj->useragent);
 
-	$newLine = $_SERVER["REMOTE_ADDR"] . "," . $obj->id . "," . $obj->startTime 
-		. "," . $obj->stopTime . "," . $obj->runtime . "," . $obj->url . "," . $obj->useragent
-		. "," . $obj->perceivedTime . "," . $obj->pageRenderTime . "," . $obj->roundTripTime 
-		. "," . $obj->tcpConnectionTime . "," . $obj->dnsLookupTime . "," . $obj->cacheTime
-		. "," . $obj->redirectTime;
+    $newLine = $_SERVER["REMOTE_ADDR"] . "," . $obj->id . "," . $obj->startTime 
+        . "," . $obj->stopTime . "," . $obj->runtime . "," . $obj->url . "," . $obj->useragent
+        . "," . $obj->perceivedTime . "," . $obj->pageRenderTime . "," . $obj->roundTripTime 
+        . "," . $obj->tcpConnectionTime . "," . $obj->dnsLookupTime . "," . $obj->cacheTime
+        . "," . $obj->redirectTime;
 
-	appendToFile($newLine, $file);
+    appendToFile($newLine, $file);
 }
 
 function cleanCommas($data) {
-	return implode("", explode(",", $data));
+    return implode("", explode(",", $data));
 }
 
 ?>
